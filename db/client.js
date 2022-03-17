dee97820b3915553e645cda149b4014fab1a1cea
PJWitt <pwitt0068@gmail.com>
2022-03-17 19:27:38 -0400

    databae done, working on API

--- db/client.js
+++ db/client.js
@@ -1,2 +1,8 @@
 // build and export your unconnected client here
+const { Client } = require("pg"); // imports the pg module
 
+const client = new Client(
+  process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev"
+);
+
+module.exports = client;
